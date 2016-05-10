9f7f1de7f8b28969b82e3256caca28884cd2b30c
jmkiley <jordankiley@gmail.com>
2016-05-10 10:43:20 -0400

    Done.

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,40 @@
  
  */
 
+- ( NSString * )stringWithDeliLine:(NSArray *)deliLine {
+    
+    NSMutableString *stringWithDeliLine= [ @"" mutableCopy];
+    
+    if ([ deliLine count ] == 0) {
+        [ stringWithDeliLine appendFormat:@"The line is currently empty." ];
+        NSLog(@"%@", stringWithDeliLine);
+    }
+    else {
+            [ stringWithDeliLine appendFormat:@"The line is:" ];
+        for ( NSUInteger i = 0; i < [ deliLine count ]; i++ ) {
+            NSUInteger spot = i + 1;
+            [ stringWithDeliLine appendFormat:@"\n%lu. %@", spot, deliLine[i] ];
+        }
+        NSLog(@"%@", stringWithDeliLine);
+    }
+    return stringWithDeliLine;
+    
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    
+    [ deliLine addObject:name ];
+    
+}
+
+- (NSString * )serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *nowServing = @"";
+    if ([ deliLine count ] > 0) {
+        nowServing = deliLine[0];
+        [ deliLine removeObjectAtIndex:0 ];
+    }
+    return nowServing;
+    return deliLine;
+}
+
 @end
